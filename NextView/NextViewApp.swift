50071c0a73042486c61cc1c527842cd6dac0f3f4
ToyohikoUchida <tyhkucd@gmail.com>
2024-08-31 13:23:53 +0900

    Initial Commit

---
+++ NextView/NextViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  NextViewApp.swift
+//  NextView
+//
+//  Created by 内田豊彦 on 2024/08/31.
+//
+
+import SwiftUI
+
+@main
+struct NextViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
